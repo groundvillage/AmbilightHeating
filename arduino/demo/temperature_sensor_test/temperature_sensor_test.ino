8923fd20f287ff58033f66fd2e67beed1ed94649
Tamara <bernhardt.tamara@gmx.de>
2016-11-03 14:06:21 +0100

    readded renamed files - stupid me..

---
+++ arduino/demo/temperature_sensor_test/temperature_sensor_test.ino
@@ -0,0 +1,27 @@
+#include <DallasTemperature.h>
+#include <OneWire.h>
+
+// Data wire is plugged into pin 3 on the Arduino
+#define ONE_WIRE_BUS 3
+ 
+// Setup a oneWire instance to communicate with any OneWire devices 
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+ 
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+ 
+void setup(void)
+{
+   // Start up the library
+  Serial.begin(9600);
+  sensors.begin();
+}
+ 
+ 
+void loop(void)
+{
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    Serial.println(sensors.getTempCByIndex(0));
+    delay(500);
+}
