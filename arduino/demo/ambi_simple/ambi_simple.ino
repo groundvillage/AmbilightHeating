007dcdbb1a9dd3855adc55a0f6722586ae28000c
Tamara <bernhardt.tamara@gmx.de>
2016-11-14 18:29:12 +0100

    checked out json parser and wrote demo which is working with the app_dummy python script

--- arduino/demo/ambi_simple/ambi_simple.ino
+++ arduino/demo/ambi_simple/ambi_simple.ino
@@ -1,3 +1,6 @@
+#include <Bridge.h>
+#include <BridgeServer.h>
+#include <BridgeClient.h>
 #include <DallasTemperature.h>
 #include <Wire.h>
 #include <OneWire.h>
@@ -7,18 +10,23 @@
   #include <avr/power.h>
 #endif
 
-#define LUMI_PIN 8
+#define PIXELS_PIN 8
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS 12 // LEDs
 #define TEMPERATURE_PIN 7 // Temperature
 #define DELAY_VAL 2000
 #define BRIGHTNESS(a,b) ((((a)/(b))- 255) * (-1))
+#define PORT 666
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); // LED
+BridgeServer server(PORT);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIXELS_PIN, NEO_GRB + NEO_KHZ800); // LED
 TSL2561 tsl(TSL2561_ADDR_FLOAT); // Light sensor
 OneWire oneWire(TEMPERATURE_PIN); // Temperature
 DallasTemperature sensors(&oneWire); // Temperature
 float brightness_mapping = 1000/255;
+uint8_t r = 255;
+uint8_t g = 255;
+uint8_t b = 255;
 
 void setup(void) {
   Serial.begin(9600);
@@ -41,9 +49,37 @@ void setup(void) {
     if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
   #endif
   pixels.begin();
-}
+
+  //++++++++++ TCP Server ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  Bridge.begin();
+  server.noListenOnLocalhost();
+  server.begin();
+  }
 
 void loop(void) {
+
+  // ++++++++++ LISTEN FOR COMMANDS ++++++++++++++++++++++++++++++++++++++++++++++++
+  BridgeClient client = server.accept();
+
+  String response;
+  if(client.connected()){
+    Serial.println("Connected with Client!");
+    while(client.connected()){
+      if(client.available()){
+        char cmd = client.read();
+        if(cmd == '\n' ||  cmd == '\0'){
+          break;
+        } else {
+          response += String(cmd);
+        }
+      }
+    }
+    client.stop();
+    Serial.println(response);
+    r = (uint8_t)((response >> 16) & 0xFF) / 255;  // Extract the RR byte
+    g = (uint8_t) ((response >> 8) & 0xFF) / 255;   // Extract the GG byte
+    b = (uint8_t) ((response) & 0xFF) / 255;
+  }
   
   // ++++++++++ LIGHT SENSOR ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   uint16_t x = tsl.getLuminosity(TSL2561_VISIBLE);
@@ -58,13 +94,13 @@ void loop(void) {
 
   // +++++++++ TEMPERATURE SENSOR +++++++++++++++++++++++++++++++++++++++++++++++++++
   sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println(sensors.getTempCByIndex(0));
+  //Serial.println(sensors.getTempCByIndex(0));
 
   
   for(int i=0;i<NUMPIXELS;i++){
 
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(50,0,0)); // Moderately bright green color.
+    pixels.setPixelColor(i, pixels.Color(r,g,b));
     pixels.show(); // This sends the updated pixel color to the hardware.
   }
   
--- arduino/demo/json_parser/json_parser.ino
+++ arduino/demo/json_parser/json_parser.ino
@@ -1,9 +1,12 @@
+#include <ArduinoJson.h>
 #include <Bridge.h>
 #include <BridgeServer.h>
 #include <BridgeClient.h>
 
 #define PORT 666
+
 BridgeServer server(PORT);
+DynamicJsonBuffer jsonBuffer;
 
 void setup() {
   Serial.begin(9600);
@@ -15,11 +18,10 @@ void setup() {
 void loop() {
 
   BridgeClient client = server.accept();
-
-  String response;
-  short i = 0;
+  
   if(client.connected()){
     Serial.println("Connected with Client!");
+    String response;
     while(client.connected()){
       if(client.available()){
         char cmd = client.read();
@@ -30,8 +32,24 @@ void loop() {
         }
       }
     }
-  client.stop();
-  Serial.println(response);
+    client.stop();
+    Serial.println(response);
+    JsonObject& root = jsonBuffer.parseObject(response);
+    if (root.success()) {
+      const char* sensor = root["sensor"];
+      long time = root["time"];
+      double latitude = root["data"][0];
+      double longitude = root["data"][1];
+
+      // Print values.
+      Serial.println(sensor);
+      Serial.println(time);
+      Serial.println(latitude, 6);
+      Serial.println(longitude, 6);
+    } else {
+      Serial.println("parseObject() failed");
+      return;
+    }
   }
 
   delay(1000);
